807c5564fe2b423d3bd2d01857efb2b407e301ab
Vojd85 <i7bicak@mail.ru>
2023-07-15 16:25:53 +0300

    Результат

--- HW_Sem6/Modules/chess.py
+++ HW_Sem6/Modules/chess.py
@@ -20,9 +20,34 @@ def no_fight(queens_coor: tuple) -> bool:
     list_row = [x[0] for x in queens_coor]
     list_col = [y[1] for y in queens_coor]
     for queen in queens_coor:
-        if list_row.count(queen[0]) != 1 or list_col.count(queen[1]) != 1 \
-            or queen[0] in list_col or queen[1] in list_row:
+        if list_row.count(queen[0]) != 1 or list_col.count(queen[1]) != 1:
             return False
+        if queen[0] > queen[1]:
+            temp = queen[0] - queen[1]
+            coor1, coor2 = temp, 0
+        else:
+            temp = queen[1] - queen[0]
+            coor1, coor2 = 0, temp
+        while coor1 < BOARD and coor2 < BOARD:
+            temp_queen = (coor1, coor2)
+            if temp_queen != queen and temp_queen in queens_coor:
+                return False
+            coor1 += 1
+            coor2 += 1
+        coor1, coor2 = queen[0], queen[1]
+        while coor1 < BOARD and coor2 >= 0:
+            temp_queen = (coor1, coor2)
+            if temp_queen != queen and temp_queen in queens_coor:
+                return False
+            coor1 += 1
+            coor2 -= 1
+        coor1, coor2 = queen[0], queen[1]
+        while coor1 >= 0 and coor2 < BOARD:
+            temp_queen = (coor1, coor2)
+            if temp_queen != queen and temp_queen in queens_coor:
+                return False
+            coor1 -= 1
+            coor2 += 1
     return True
 
 def good_position(count, count_queens):
@@ -31,6 +56,7 @@ def good_position(count, count_queens):
         temp = tuple((ri(MIN_RND,MAX_RND), ri(MIN_RND,MAX_RND)) for _ in range(count_queens))
         if no_fight(temp):
             result.append(temp)
+            print(temp)
             count -= 1
     return result
 
